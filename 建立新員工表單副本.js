06795b8716b7de70d2db01ebb476244e3f021fa0
zhaoen <zhaoen@capsulecorporation.cc>
2026-01-28 16:02:59 +0800

    fix: 處理日期過長的問題

--- 建立新員工表單副本.js
+++ 建立新員工表單副本.js
@@ -7,7 +7,7 @@ function createNewSpreadsheet() {
         const ss = SpreadsheetApp.getActiveSpreadsheet();   
         const sheet = ss.getActiveSheet();
         const employeeName = sheet.getRange("B2").getValue(); // 假設姓名在 B2 儲存格
-        const startDate = sheet.getRange("B6").getValue(); // 假設就職日期在 B3 儲存格
+        const startDate = (rawDate instanceof Date) ? Utilities.formatDate(rawDate, "GMT+8", "yyyy-MM-dd") : rawDate;
         const newFileName = employeeName + "_" + startDate;
 
         // 3. 取得範本檔案並建立副本
