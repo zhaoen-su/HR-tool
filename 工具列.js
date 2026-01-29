872bf02711275e8a8f9e0d614808cc050bab7442
zhaoen <zhaoen@capsulecorporation.cc>
2026-01-29 12:58:49 +0800

    feat: 新增功能按鈕

--- 工具列.js
+++ 工具列.js
@@ -2,8 +2,9 @@ function onOpen() {
   var ui = SpreadsheetApp.getUi(); // 取得 UI 環境
   
   ui.createMenu('✨功能表')      // 設定功能列顯示的名稱
-    .addItem('建立日期', 'ADJUST_WORKDAY')
+    .addItem('建立／更新日期(WIP)', 'ADJUST_WORKDAY')
     .addSeparator()
-    .addItem('建立新員工表單副本', 'createNewSpreadsheet') // (顯示名稱, 執行的函數名稱)
-    .addToUi();                  // 必須呼叫這行才會渲染到畫面上
+    .addItem('建立新員工表單副本', 'createNewSpreadsheet')
+    .addItem('建立行事曆(WIP)','createCalendar')
+    .addToUi();
 }
