a210bbc2fe71af8ae191bc88faeb7c1b97f10c3f
zhaoen <zhaoen@capsulecorporation.cc>
2026-01-29 15:52:21 +0800

    fix: 修改函式名稱

--- 工具列.js
+++ 工具列.js
@@ -2,7 +2,7 @@ function onOpen() {
   var ui = SpreadsheetApp.getUi(); // 取得 UI 環境
   
   ui.createMenu('✨功能表')      // 設定功能列顯示的名稱
-    .addItem('建立／更新日期(WIP)', 'ADJUST_WORKDAY')
+    .addItem('建立／更新日期(WIP)', 'generateDate')
     .addSeparator()
     .addItem('建立新員工表單副本', 'createNewSpreadsheet')
     .addItem('建立行事曆(WIP)','createCalendar')
