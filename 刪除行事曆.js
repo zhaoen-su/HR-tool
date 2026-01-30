e06570b96ea9833b24bc72b7b2af6b29060cde5f
zhaoen <zhaoen@capsulecorporation.cc>
2026-01-30 17:01:03 +0800

    feat: 新增刪除功能

---
+++ 刪除行事曆.js
@@ -0,0 +1,23 @@
+
+function deleteCalendar() {
+    // 1. 取得基本資料
+    const scheduelData = sheet.getRange("F3:F11").getValues();
+    const calendar = CalendarApp.getDefaultCalendar();
+
+    scheduelData.forEach((row) => {
+        const eventId = row[0];
+
+        if (eventId) {
+            try {
+                const existingEvent = calendar.getEventById(eventId);
+                if (existingEvent) {
+                    existingEvent.deleteEvent();
+                }
+            } catch (e) {
+                console.log(`刪除舊事件失敗 (可能已不存在): ` + e.toString());
+            }
+        }
+    });
+
+    SpreadsheetApp.getUi().toast("行事曆同步完成！");
+}
