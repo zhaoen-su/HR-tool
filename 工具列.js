db3f9f02fbfa08caf3360911a86937cf4baa5be6
zhaoen <zhaoen@capsulecorporation.cc>
2026-01-30 15:12:36 +0800

    doc: 更改檔案名稱

--- 工具列.js
+++ 工具列.js
@@ -2,9 +2,9 @@ function onOpen() {
   var ui = SpreadsheetApp.getUi(); // 取得 UI 環境
   
   ui.createMenu('✨功能表')      // 設定功能列顯示的名稱
-    .addItem('建立／更新日期(WIP)', 'generateDate')
+    .addItem('建立／更新日期', 'generateDate')
     .addSeparator()
     .addItem('建立新員工表單副本', 'createNewSpreadsheet')
-    .addItem('建立行事曆(WIP)','createCalendar')
+    .addItem('建立行事曆','createCalendar')
     .addToUi();
 }
