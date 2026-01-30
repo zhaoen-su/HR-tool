92da371734ba850609ce2c92182c0788f3b2ff7a
zhaoen <zhaoen@capsulecorporation.cc>
2026-01-30 18:09:56 +0800

    fix: 修改超出範圍的儲存格

--- 刪除行事曆.js
+++ 刪除行事曆.js
@@ -3,7 +3,7 @@ function deleteCalendar() {
     // 1. 取得基本資料
     const ss = SpreadsheetApp.getActiveSpreadsheet();
     const sheet = ss.getActiveSheet();
-    const scheduelData = sheet.getRange("F2:F11").getValues();
+    const scheduelData = sheet.getRange("F2:F10").getValues();
     const calendar = CalendarApp.getDefaultCalendar();
 
     scheduelData.forEach((row) => {
@@ -16,7 +16,7 @@ function deleteCalendar() {
                     existingEvent.deleteEvent();
                 }
 
-                sheet.getRange("F2:F11").clearContent();
+                sheet.getRange("F2:F10").clearContent();
             } catch (e) {
                 console.log(`刪除舊事件失敗 (可能已不存在): ` + e.toString());
             }
