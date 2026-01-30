3c8e8a00c470ce35d4cb105864073d70e77636bf
zhaoen <zhaoen@capsulecorporation.cc>
2026-01-30 17:03:47 +0800

    fix: 功能表新增刪除按鈕

--- 工具列.js
+++ 工具列.js
@@ -5,6 +5,8 @@ function onOpen() {
     .addItem('建立／更新日期', 'generateDate')
     .addSeparator()
     .addItem('建立新員工表單副本', 'createNewSpreadsheet')
+    .addSeparator()
     .addItem('建立行事曆','createCalendar')
+    .addItem('刪除行事曆','deleteCalendar')
     .addToUi();
 }
