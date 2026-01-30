24ea101e58b06f07ec78a2c05fc6a8ae303a6636
zhaoen <zhaoen@capsulecorporation.cc>
2026-01-30 17:37:50 +0800

    fix: 修正讓eventId一起被移除

--- 刪除行事曆.js
+++ 刪除行事曆.js
@@ -1,7 +1,9 @@
 
 function deleteCalendar() {
     // 1. 取得基本資料
-    const scheduelData = sheet.getRange("F3:F11").getValues();
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet = ss.getActiveSheet();
+    const scheduelData = sheet.getRange("F2:F11").getValues();
     const calendar = CalendarApp.getDefaultCalendar();
 
     scheduelData.forEach((row) => {
@@ -13,11 +15,11 @@ function deleteCalendar() {
                 if (existingEvent) {
                     existingEvent.deleteEvent();
                 }
+
+                sheet.getRange("F2:F11").clearContent();
             } catch (e) {
                 console.log(`刪除舊事件失敗 (可能已不存在): ` + e.toString());
             }
         }
     });
-
-    SpreadsheetApp.getActiveSpreadsheet().toast("行事曆刪除完成！");
 }
