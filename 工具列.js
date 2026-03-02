0af3993e2666f76bd3b1d8064de777093f16403b
zhaoen <zhaoen@capsulecorporation.cc>
2026-03-02 18:15:04 +0800

    refactor: 功能列表新增數字標註

--- 工具列.js
+++ 工具列.js
@@ -2,11 +2,11 @@ function onOpen() {
   var ui = SpreadsheetApp.getUi(); // 取得 UI 環境
   
   ui.createMenu('✨功能表')      // 設定功能列顯示的名稱
-    .addItem('建立／更新日期（迴避假日）', 'generateDate')
+    .addItem('1. 建立／更新日期（迴避假日）', 'generateDate')
     .addSeparator()
-    .addItem('建立行事曆','createCalendar')
+    .addItem('2. 建立行事曆','createCalendar')
     .addItem('刪除行事曆','deleteCalendar')
     .addSeparator()
-    .addItem('建立新員工表單副本', 'createNewSpreadsheet')
+    .addItem('3. 建立新員工表單副本', 'createNewSpreadsheet')
     .addToUi();
 }
